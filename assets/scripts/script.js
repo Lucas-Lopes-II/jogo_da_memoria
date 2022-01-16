1ac58be8df3948fe0fc271a571a33c74c0838d80
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2022-01-16 10:53:14 -0300

    Tranferindo para game aparteda lógica de negócio

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,6 @@
 const FRONT = 'card_front'
 const BACK = 'card_back'
 
-let techs = [
-    'bootstrap',
-    'css',
-    'electron',
-    'firebase',
-    'html',
-    'javascript',
-    'jquery',
-    'mongo',
-    'node',
-    'react',
-];
-
-let cards = null;
-
 startGame();
 
 function startGame(){
@@ -78,35 +63,6 @@ function shuffleCards(cards){
 }
 
 createCardsFromTechs(techs);
-
-function createCardsFromTechs(techs){
-    let cards= [];
-
-    techs.forEach((tech) => {
-        cards.push(createPairFromTech(tech));
-    });
-    return cards.flatMap(pair => pair);
-    
-}
-
-function createPairFromTech(tech){
-    return [
-        {
-            id: createIdWithTech(tech),
-            icon: tech,
-            flipped: false
-        }, 
-        {
-            id: createIdWithTech(tech),
-            icon: tech,
-            flipped: false
-        }
-    ]
-}
-
-function createIdWithTech(tech){
-    return tech + parseInt(Math.random() * 1000)
-}
 
 function flipCard(){
     this.classList.add('flip');

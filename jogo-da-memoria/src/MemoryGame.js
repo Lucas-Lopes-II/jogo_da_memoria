9d443e411dbf3d1c85a66e2f50d33f884d718a97
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2022-03-05 15:27:50 -0300

    Criação da tela de Game Over

--- jogo-da-memoria/src/MemoryGame.js
+++ jogo-da-memoria/src/MemoryGame.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import game from './game';
+import GameOver from './components/GameOver';
 
 export default function MemoryGame() {
+    const [gameOver, setGameOver] = useState(true);
+
+    const handleRestart = () => {
+        setGameOver(false);
+    }
+
   return (
     <>
 
+    <GameOver show={gameOver} handleRestart={handleRestart}/>
     </>
   )
 }
